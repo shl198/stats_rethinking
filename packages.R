1cfbb9a6d3acc16e805014fa2f48261586ba83d9
shl198 <shangzhong0619@gmail.com>
2023-05-20 04:40:16 +0000

    add stats_rethink course

---
+++ packages.R
@@ -0,0 +1,14 @@
+packages <- c('brms', 'tidybayes')
+
+install.packages(packages, dependencies = T)
+
+
+devtools::install_github("stan-dev/cmdstanr")
+devtools::install_github("EdwinTh/dutchmasters")
+devtools::install_github("gadenbuie/ggpomological")
+devtools::install_github("GuangchuangYu/ggtree")
+devtools::install_github("rmcelreath/rethinking")
+devtools::install_github("UrbanInstitute/urbnmapr")
+
+
+packages = c('ggfun')
